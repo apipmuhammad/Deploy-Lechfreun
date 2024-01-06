fe35a64dc9aeb1bd07077a73c8c5b4f543b29f1c
apipmuhammad <apipnaxggs@gmail.com>
2024-01-06 23:23:42 +0700

    update terbaru

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from keras.utils import pad_sequences
 from keras.models import load_model
 from joblib import load
 
+
 st.image('image/a.jpg')
 st.title('Automatic Systematic Literature Review')
 st.write("""Selamat datang di Automatic Systematic Literature Review. Aplikasi ini menggunakan
@@ -29,44 +30,6 @@ with col2:
    st.subheader("Screening Artikel")
    st.write("""Aplikasi ini dapat membantu para dokter dan 
             peneliti untuk membantu screening artikel dengan efisien""")
-   
-model = load('model_1.joblib')
-st.header('Aplikasi Klasifikasi Keyword')  
-model = load('model_1.joblib')
-vocab = pickle.load(open('kbest_feature.pickle', 'rb'))
-
-tf_idf_vec = TfidfVectorizer(vocabulary=set(vocab))
-user_input = st.text_input('Masukkan Title :')
-user_input1 = st.text_input('Masukkan Abstract :')  
-tfidf = tf_idf_vec.fit_transform([user_input], [user_input1])
-prediksi = model.predict(tfidf)
-
-if st.button("prediksi"):
-
-    if prediksi == 0:
-        prediction = 'Exclude'
-    elif prediksi == 1:
-        prediction = 'Include'
-
-    st.write("Hasil prediksi: adalah", prediction)
     
-st.header('Our Teams')
-st.write("""Dalam proses pembuatan website aplikasi klasifikasi keyword 
-         disini kami terdiri dari 3 orang""")
 
-col1, col2, col3 = st.columns(3)
-with col1:
-   original_title = '<p font-size: 40px;"></p>'
-   st.markdown(original_title, unsafe_allow_html=True)
-   st.image('image/Picture1.png', caption='Dikco Agung Prasetyo')
-
-with col2:
-   original_title = '<p font-size: 40px; "></p>'
-   st.markdown(original_title, unsafe_allow_html=True)
-   st.image('image/Picture2.png', caption='Muhamad Ivan Fadhillah')
-
-with col3:
-   original_title = '<p font-size: 40px;"></p>'
-   st.markdown(original_title, unsafe_allow_html=True)
-   st.image('image/Picture3.png', caption='Muhammad Soleh Apriadi')
 
